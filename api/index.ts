9e6e38d05451d62471bc67a2769c602ab0e5ac1a
Apurva <apurva@devkraft.co.in>
2026-05-21 14:09:37 +0530

    Add Vercel deployment config (PostgreSQL, serverless handler)

---
+++ api/index.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../src/app.module';
+import { TransformInterceptor } from '../src/common/interceptors/transform.interceptor';
+import { HttpExceptionFilter } from '../src/common/filters/http-exception.filter';
+import * as express from 'express';
+import type { Request, Response } from 'express';
+
+const server = express();
+let isInitialized = false;
+
+async function bootstrap() {
+  if (!isInitialized) {
+    const adapter = new ExpressAdapter(server);
+    const app = await NestFactory.create(AppModule, adapter, { logger: ['error', 'warn'] });
+
+    app.enableCors({ origin: true, credentials: true });
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: false }));
+    app.useGlobalInterceptors(new TransformInterceptor());
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.setGlobalPrefix('api');
+
+    await app.init();
+    isInitialized = true;
+  }
+}
+
+export default async function handler(req: Request, res: Response) {
+  await bootstrap();
+  server(req, res);
+}
